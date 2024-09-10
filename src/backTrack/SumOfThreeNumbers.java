37fb6950dbbd29e283fe03fb23724ab4aedda4fd
sunpuxi <2531821176@qq.com>
2024-09-10 11:27:10 +0800

    dp

--- src/backTrack/SumOfThreeNumbers.java
+++ src/backTrack/SumOfThreeNumbers.java
@@ -15,24 +15,27 @@ public class SumOfThreeNumbers {
     public List<List<Integer>> threeSum(int[] nums) {
         // 排序
         Arrays.sort(nums);
-        // 结果集合
+        // 结果集
         List<List<Integer>> res = new ArrayList<>();
         // 遍历
-        for (int k = 0; k < nums.length - 2; k++) {
-            // 最小的元素都大于0，那么三数之和必定不可能为0，后面也没有必要继续遍历了
-            if (nums[k] > 0) break;
-            // 如果 k 大于0,并且第k个元素等于他的前一个元素，那么进行去重操作
-            if (k > 0 && nums[k] == nums[k - 1]) continue;
+        for(int k=0;k<nums.length-2;k++){
+            // 如果当前元素大于0，直接退出循环，后面不存在比他小的元素，也就不会三数之和为0了。
+            if(nums[k] > 0) break;
+            // 去重
+            if(k>0 && nums[k] == nums[k-1]) continue;
             int i = k + 1;
-            int j = nums.length - 1;
-            while (i < j) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int j = nums.length-1;
+            while(i < j){
+                int sum = nums[k] + nums[i] + nums[j];
                 if (sum < 0){
+                    // sum小于0，左边的元素小了，向右移动的同时进行去重操作
                     while(i < j && nums[i] == nums[++i]);
                 } else if (sum > 0) {
+                    // sum大于0，右边的元素大了，向后移动的同时去重
                     while(i < j && nums[j] == nums[--j]);
                 }else{
-                    res.add(new ArrayList<>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    // 收集结果的同时改变两个指针的位置，并去重
+                    res.add(new ArrayList<>(Arrays.asList(nums[k],nums[i],nums[j])));
                     while(i < j && nums[i] == nums[++i]);
                     while(i < j && nums[j] == nums[--j]);
                 }
