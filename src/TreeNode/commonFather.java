8554c3367cbd60ee06dbabbc8063475182fea47f
sunpuxi <2531821176@qq.com>
2024-09-18 09:45:59 +0800

    二分查找牛逼

--- src/TreeNode/commonFather.java
+++ src/TreeNode/commonFather.java
@@ -6,21 +6,23 @@ package src.TreeNode;
 public class commonFather {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // 特殊情况
-        if(root == null || root == p || root == q) return root;
+        if (root == null || root == p || root == q) return root;
         // 依次递归左边和右边
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         // 如果左右两边都有返回当前节点即可
-        if (left!=null && right!=null) return root;
+        if (left != null && right != null) return root;
         // 如果只有一边
-        if (left!=null) return left;
+        if (left != null) return left;
         else return right;
     }
+
     // 二叉搜索树中的情况
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         int val = root.val;
-        if(p.val < val && q.val < val) return lowestCommonAncestor(root.left,p,q);
-        if(p.val > val && q.val > val) return lowestCommonAncestor(root.right,p,q);
+        if (p.val < val && q.val < val) return lowestCommonAncestor(root.left, p, q);
+        if (p.val > val && q.val > val) return lowestCommonAncestor(root.right, p, q);
+        // 一大一小返回 root 根节点
         return root;
     }
 
