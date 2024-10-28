5fe6b91444428d4802852e285094d9cc50f039e4
sunpuxi <2531821176@qq.com>
2024-10-28 15:32:13 +0800

    回文链表写法更新

--- src/ListNode/isHuiWenListNodeFirst.java
+++ src/ListNode/isHuiWenListNodeFirst.java
@@ -7,39 +7,37 @@ package src.ListNode;
  */
 public class isHuiWenListNodeFirst {
     public  boolean isOrNot(ListNode head){
-        if (head == null || head.next == null) return true;
-        ListNode left = head;
-        ListNode right;
+        // 1、反转后半部分的链表
+        // 2、从前往后遍历的同事也从后往前遍历，依次比较两个元素的值即可
+        if(head == null || head.next == null) return true;
+        // 如果只有两个节点
+        if(head.next != null && head.next.next == null) return head.next.val == head.val;
+        // 其他情况
         ListNode slow = head;
         ListNode fast = head;
-        //如果节点个数是奇数，那么fast最终指向的位置是最后一个节点
-        //如果节点个数是偶数，那么fast指向的位置是最后一个节点的前一个元素，此时还需要让fast后移一位
-        while(fast.next != null && fast.next.next != null){
+        // 先找到中间位置和末尾位置的节点
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if(fast.next!=null && fast.next.next==null) fast = fast.next;
         }
-        //记录下来末尾节点
-        //开始反转链表,反转 slow 之后的节点
-        right = slow.next;
-        ListNode tmp = right.next;
-        slow.next = null;
+        // 开始反转后半部分,slow 后面的的链表
+        ListNode right = slow.next;
+        ListNode temp = right.next;
         right.next = slow;
+        slow.next = null;
         slow = right;
-        right = tmp;
-        //循环结束之后，right为null，slow指向最后一个节点，至此后半部分的链表反转完成
+        right = temp;
         while(right != null){
-            tmp = right.next;
+            temp = right.next;
             right.next = slow;
             slow = right;
-            right = tmp;
+            right = temp;
         }
-        right = slow;
-        //开始比较元素的值
-        while(left!=null){
-            if(left.val!= right.val) return false;
-            left = left.next;
-            right = right.next;
+        // 比较
+        while(head != null && slow != null){
+            if(head.val != slow.val) return false;
+            head = head.next;
+            slow = slow.next;
         }
         return true;
     }
