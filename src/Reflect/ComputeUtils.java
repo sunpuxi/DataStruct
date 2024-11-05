756951ae295aef0d1176ce42824753b48fd00872
sunpuxi <2531821176@qq.com>
2024-11-05 14:44:40 +0800

    回文链表写法更新

--- src/Reflect/ComputeUtils.java
+++
@@ -1,8 +0,0 @@
-package src.Reflect;
-
-public  class ComputeUtils {
-
-    public int add(int a,int b){
-        return a + b;
-    }
-}
--- src/Reflect/TargetClass.java
+++
@@ -1,8 +0,0 @@
-package src.Reflect;
-
-public class TargetClass {
-    // 这是一个目标方法，用于反射调用
-    public int targetMethod(int number) {
-        return number * number;
-    }
-}
