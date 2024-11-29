5678fba5a638014c33566fea84ddb56cd8d78a5b
sunpuxi <2531821176@qq.com>
2024-11-29 15:01:08 +0800

    feat:单调栈

--- java/src/单调栈/最大宽度坡.java
+++ java/src/单调栈/最大宽度坡.java
@@ -3,8 +3,13 @@ package src.单调栈;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * i ＜ j，nums【i】 ≤ nums【j】；实际的值不需要关注，将题目转换为找出满足条件的最大的 j 和最小的 i。
+ * 不再是常规的单调栈的操作
+ * 解题办法：1、找出最长的递减序列  2、从后开始往前找结果
+ * 问题：为什么这样是可行的？
+ */
 public class 最大宽度坡 {
-
     public int maxWidthRamp(int[] nums) {
         int len = nums.length;
         int max = 0;
