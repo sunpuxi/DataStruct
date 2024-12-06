634593a05ec9968932728047f4dcb32113fc9bc7
sunpuxi <2531821176@qq.com>
2024-12-06 14:46:13 +0800

    feat:常用的枚举和维护方式

--- java/src/双指针/数组中的最大数对和.java
+++ java/src/双指针/数组中的最大数对和.java
@@ -8,8 +8,6 @@ public class 数组中的最大数对和 {
      * 优化版本
      * 时间复杂度：O(n)，其中n是数组的长度。我们只需要遍历数组一次。
      * 空间复杂度：O(1)，我们使用了一个固定大小的数组来存储每个数位对应的最大值。
-     * @param nums
-     * @return
      */
     public int maxSum(int[] nums) {
         // 使用数组来存储每个数位对应的最大值，数位的范围是0-9，所以数组大小为10
@@ -42,8 +40,6 @@ public class 数组中的最大数对和 {
     /**
      * 时间：O(n*m) n 为数组的长度，m 为一个数字的长度。
      * 空间：O（m*n）
-     * @param nums
-     * @return
      */
     public int maxSum1(int[] nums) {
         // 首先要满足最大数字相等，再去谈求和的问题
@@ -51,34 +47,33 @@ public class 数组中的最大数对和 {
         // 然后每次遍历到一个数时，就看前面是否存在过跟他最大数位相同的情况，如果存在则求和计算，更新最大值，不存在则重复上面的步骤。
         Map<Integer, List<Integer>> map = new HashMap<>();
         int max = -1;
-        for(int num : nums){
+        for (int num : nums) {
             int number = getTheMaxNumber1(num);
-            if(map.containsKey(number)){
-                List<Integer> list = map.get(number);
+            List<Integer> list;
+            if (map.containsKey(number)) {
+                list = map.get(number);
                 int m = 0;
                 // 找出集合中存在的最大元素
-                for(int i:list) m = Math.max(i,m);
+                for (int i : list) m = Math.max(i, m);
                 // 更新最大值
-                max = Math.max(max,num + m);
+                max = Math.max(max, num + m);
                 // 将当前的元素添加到集合中
-                list.add(num);
-                map.put(number,list);
-            }else{
-                List<Integer> list  = new ArrayList<>();
-                list.add(num);
-                map.put(number,list);
+            } else {
+                list = new ArrayList<>();
             }
+            list.add(num);
+            map.put(number, list);
         }
         return max;
     }
 
-    public int getTheMaxNumber1(int num){
+    public int getTheMaxNumber1(int num) {
         String str = String.valueOf(num);
         char[] arr = str.toCharArray();
         int max = 0;
-        for(char ch : arr){
+        for (char ch : arr) {
             int a = ch - '0';
-            max = Math.max(a,max);
+            max = Math.max(a, max);
         }
         return max;
     }
